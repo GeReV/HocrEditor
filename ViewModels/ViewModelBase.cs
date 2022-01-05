2a53a5553f408d4b03e142f2265186646ffe6257
Amir Grozki <EMAIL>
2022-01-05 04:31:31 +0200

    Shutting up a warning.

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -4,6 +4,8 @@ namespace HocrEditor.ViewModels
 {
     public class ViewModelBase : INotifyPropertyChanged
     {
+#pragma warning disable CS0067
         public event PropertyChangedEventHandler? PropertyChanged;
+#pragma warning restore CS0067
     }
 }
