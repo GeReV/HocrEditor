9d6dd38dca1dc15ff116ed3bb7276d5183872f52
Amir Grozki <EMAIL>
2022-01-13 10:30:57 +0200

    Minor improvement to Tesseract arguments.

--- Services/TesseractService.cs
+++ Services/TesseractService.cs
@@ -15,17 +15,20 @@ namespace HocrEditor.Services
 
         public async void GetLanguages()
         {
-            var languages = await Task.Run(async () =>
-            {
-                var result = await ProcessRunner.Run(TesseractPath, "--list-langs");
+            var languages = await Task.Run(
+                async () =>
+                {
+                    var result = await ProcessRunner.Run(TesseractPath, "--list-langs");
 
-                return result.Split(new[] {'\n', '\r'}, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList();
-            });
+                    return result.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList();
+                }
+            );
         }
 
-        public async Task<string> PerformOcr(string filename, string[] languages)
-        {
-            return await ProcessRunner.Run(TesseractPath, $"{filename} stdout -l {string.Join('+', languages)} --psm 1 hocr");
-        }
+        // TODO: Determine DPI from image.
+        public async Task<string> PerformOcr(string filename, string[] languages) => await ProcessRunner.Run(
+            TesseractPath,
+            $"{filename} stdout --dpi 300 -l {string.Join('+', languages)} --psm 3 -c hocr_font_info=1 hocr"
+        );
     }
 }
