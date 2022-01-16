268706a446120756082c6948c9b5060e921c1e97
Amir Grozki <EMAIL>
2022-01-17 01:00:27 +0200

    Fixed CollectionRemoveCommand and CollectionAddCommand.

--- Commands/UndoRedo/CollectionAddCommand.cs
+++ Commands/UndoRedo/CollectionAddCommand.cs
@@ -9,13 +9,11 @@ public class CollectionAddCommand : UndoRedoCommand
     // ReSharper disable once SuggestBaseTypeForParameterInConstructor
     public CollectionAddCommand(IList sender, object child) : base(sender)
     {
-        children = new ArrayList { child };
-    }
-
-    // ReSharper disable once SuggestBaseTypeForParameterInConstructor
-    public CollectionAddCommand(IList sender, IList children) : base(sender)
-    {
-        this.children = children;
+        children = child switch
+        {
+            IList list => list,
+            _ => new ArrayList { child }
+        };
     }
 
     public override void Undo()
--- Commands/UndoRedo/CollectionRemoveCommand.cs
+++ Commands/UndoRedo/CollectionRemoveCommand.cs
@@ -9,13 +9,11 @@ public class CollectionRemoveCommand : UndoRedoCommand
     // ReSharper disable once SuggestBaseTypeForParameterInConstructor
     public CollectionRemoveCommand(IList sender, object child) : base(sender)
     {
-        children = new ArrayList { child };
-    }
-
-    // ReSharper disable once SuggestBaseTypeForParameterInConstructor
-    public CollectionRemoveCommand(IList sender, IList children) : base(sender)
-    {
-        this.children = children;
+        children = child switch
+        {
+            IList list => list,
+            _ => new ArrayList { child }
+        };
     }
 
     public override void Undo()
