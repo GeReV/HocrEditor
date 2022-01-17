b6fb0796b483b0555dd6759d8838bb447ecdde7b
Amir Grozki <EMAIL>
2022-01-17 23:23:25 +0200

    Improved merging functionality to auto-delete empty parents.

--- Commands/MergeNodesCommand.cs
+++ Commands/MergeNodesCommand.cs
@@ -63,23 +63,35 @@ public class MergeNodes : CommandBase<IList<HocrNodeViewModel>>
             commands.Add(first.Children.ToCollectionAddCommand(child));
         }
 
-        commands.Add(new DocumentRemoveNodesCommand(mainWindowViewModel.Document, rest));
-
-        // first.BBox = NodeHelpers.CalculateUnionRect(nodes);
-        commands.Add(
-            PropertyChangeCommand.FromProperty(
-                first,
-                f => f.BBox,
-                () =>
-                {
-                    var newNodes = first.Descendents.Prepend(first).ToList();
-
-                    return NodeHelpers.CalculateUnionRect(newNodes);
-                }
+        // Find all ascendants which will remain empty after the merge, and include them in the deletion.
+        var emptyAscendants = rest.SelectMany(
+                node =>
+                    // Climb the parents and keep the ones who will remain empty after their child is deleted.
+                    // If one is encountered that will not remain empty, or we arrive at the root node, stop.
+                    node.Ascendants.TakeWhile(a => a.Children.Count == 1 && !a.IsRoot)
             )
+            .ToList();
+
+        commands.Add(new DocumentRemoveNodesCommand(mainWindowViewModel.Document, rest.Concat(emptyAscendants)));
+
+        var ascendants = first.Ascendants.Prepend(first);
+
+        var updateBoundsCommands = ascendants.Select(
+            p =>
+                PropertyChangeCommand.FromProperty(
+                    p,
+                    f => f.BBox,
+                    () =>
+                    {
+                        var newNodes = p.Descendents.Prepend(p).ToList();
+
+                        return NodeHelpers.CalculateUnionRect(newNodes);
+                    }
+                )
         );
 
-        // ExecuteUndoableCommand(commands);
+        commands.AddRange(updateBoundsCommands);
+
         mainWindowViewModel.UndoRedoManager.ExecuteCommands(commands);
     }
 }
