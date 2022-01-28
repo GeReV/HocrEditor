f97ed4cb67cb97575483e4c22e3bdbc7646b6412
Amir Grozki <EMAIL>
2022-01-28 18:29:46 +0200

    Minor fix.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using HocrEditor.Controls;
 using HocrEditor.Core;
 using HocrEditor.Helpers;
