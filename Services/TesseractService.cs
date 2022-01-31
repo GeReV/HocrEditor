ecc0bde2909db4de3b08e59d9fd95bb413c9fcd9
Amir Grozki <EMAIL>
2022-01-31 03:10:16 +0200

    Initial work on OCRing a region.

--- Services/TesseractService.cs
+++ Services/TesseractService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,10 +28,36 @@ namespace HocrEditor.Services
             );
         }
 
-        // TODO: Determine DPI from image.
-        public async Task<string> PerformOcr(string filename, string[] languages) => await ProcessRunner.Run(
-            TesseractPath,
-            $"{filename} stdout --dpi 300 -l {string.Join('+', languages)} --psm 3 -c hocr_font_info=1 hocr"
-        );
+        public async Task<string> PerformOcr(string filename, string[] languages)
+        {
+            using var image = Image.FromFile(filename);
+
+            return await ProcessRunner.Run(
+                TesseractPath,
+                $"{filename} stdout --dpi {(int)image.HorizontalResolution} -l {string.Join('+', languages)} --psm 3 -c hocr_font_info=1 hocr"
+            );
+        }
+
+        public async Task<string> PerformOcrRegion(string filename, Rectangle region, string[] languages)
+        {
+            using var image = Image.FromFile(filename);
+
+            using var bitmap = new Bitmap(image);
+
+            using var cropped = bitmap.Clone(region, bitmap.PixelFormat);
+
+            var tempFile = new FileInfo(Path.GetTempFileName());
+
+            cropped.Save(tempFile.FullName, ImageFormat.Bmp);
+
+            var result = await ProcessRunner.Run(
+                TesseractPath,
+                $"{tempFile.FullName} stdout --dpi {(int)image.HorizontalResolution} -l {string.Join('+', languages)} --psm 3 hocr"
+            );
+
+            tempFile.Delete();
+
+            return result;
+        }
     }
 }
