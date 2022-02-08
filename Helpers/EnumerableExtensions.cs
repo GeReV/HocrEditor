37c5fa43a72ee11ab5c0cf4d993ed3b633602a17
Amir Grozki <EMAIL>
2022-02-08 22:31:28 +0200

    Added IEnumerable<T>.Partition extension method.

---
+++ Helpers/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HocrEditor.Helpers;
+
+public static class EnumerableExtensions
+{
+    public static (IEnumerable<T>, IEnumerable<T>) Partition<T>(this IEnumerable<T> enumerable, Predicate<T> predicate)
+    {
+        var a = new List<T>();
+        var b = new List<T>();
+
+        foreach (var item in enumerable)
+        {
+            if (predicate(item))
+            {
+                a.Add(item);
+            }
+            else
+            {
+                b.Add(item);
+            }
+        }
+
+        return (a, b);
+    }
+}
