acfd8e4c4b998965a1385a96935972d52bf5b544
Amir Grozki <EMAIL>
2022-02-10 00:01:28 +0200

    Added CountBy extension.

--- Helpers/EnumerableExtensions.cs
+++ Helpers/EnumerableExtensions.cs
@@ -24,4 +24,37 @@ public static class EnumerableExtensions
 
         return (a, b);
     }
+
+    public static IDictionary<TRet, uint> CountBy<TSource, TRet>(this IEnumerable<TSource> enumerable, Func<TSource, TRet> selector) where TRet : notnull
+    {
+        var dictionary = new Dictionary<TRet, uint>();
+
+        foreach (var item in enumerable)
+        {
+            var key = selector(item);
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key] += 1;
+            }
+            else
+            {
+                dictionary[key] = 1;
+            }
+        }
+
+        if (typeof(TRet).IsEnum)
+        {
+            var values = Enum.GetValues(typeof(TRet));
+
+            foreach (TRet value in values)
+            {
+                if (!dictionary.ContainsKey(value))
+                {
+                    dictionary[value] = 0;
+                }
+            }
+        }
+
+        return dictionary;
+    }
 }
