efb767e19f3ce40a86e7f8d034807abf67107791
Amir Grozki <amirgrozki@gmail.com>
2024-12-10 12:48:47 +0200

    Extracted EventArgsCache from RangeObservableCollection.

---
+++ HocrEditor/Core/EventArgsCache.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace HocrEditor.Core;
+
+/// <remarks>
+/// To be kept outside <see cref="ObservableCollection{T}"/>, since otherwise, a new instance will be created for each generic type used.
+/// </remarks>
+internal static class EventArgsCache
+{
+    internal static readonly PropertyChangedEventArgs AnyPropertyChanged = new(string.Empty);
+
+    internal static readonly PropertyChangedEventArgs CountPropertyChanged = new("Count");
+
+    internal static readonly PropertyChangedEventArgs IndexerPropertyChanged = new("Item[]");
+
+    internal static readonly NotifyCollectionChangedEventArgs ResetCollectionChanged =
+        new(NotifyCollectionChangedAction.Reset);
+}
