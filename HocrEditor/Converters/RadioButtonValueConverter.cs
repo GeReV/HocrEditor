16e98cbb58764032246bc7f2e91c7b6bc4addd3f
Amir Grozki <amirgrozki@gmail.com>
2025-02-05 08:58:32 +0200

    Added RadioButton-related classes.

---
+++ HocrEditor/Converters/RadioButtonValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace HocrEditor.Converters;
+
+public class RadioButtonValueConverter(object optionValue) : MarkupExtension, IValueConverter
+{
+    private object OptionValue { get; } = optionValue;
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value != null && value.Equals(OptionValue);
+
+    public object ConvertBack(object? isChecked, Type targetType, object? parameter, CultureInfo culture)
+        => (bool)(isChecked ?? false) // Is this the checked RadioButton? If so...
+            ? OptionValue // Send 'OptionValue' back to update the associated binding. Otherwise...
+            : Binding.DoNothing; // Return Binding.DoNothing, telling the binding 'ignore this change'
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+        => this;
+}
---
+++ HocrEditor/Helpers/RadioButtonBinding.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Data;
+using HocrEditor.Converters;
+
+namespace HocrEditor.Helpers;
+
+public class RadioButtonBinding : Binding
+{
+    public RadioButtonBinding(string path, object optionValue)
+        : base(path)
+    {
+        Converter = new RadioButtonValueConverter(optionValue);
+    }
+}
